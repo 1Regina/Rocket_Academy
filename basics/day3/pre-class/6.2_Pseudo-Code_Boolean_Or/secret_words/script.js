44df1b3af9b78296b1c2955bad06f9dd4d51d034
1Regina <1reginacheong@gmail.com>
2021-10-09 00:17:26 +0800

    secret words solution

---
+++ basics/day3/pre-class/6.2_Pseudo-Code_Boolean_Or/secret_words/script.js
@@ -0,0 +1,14 @@
+let main = function (input) {
+  // Set a default value for myOutputValue
+  let myOutputValue = 'hello world';
+  // If input is our secret phrase, change the value of myOutputValue
+  if (input == 'palatable papaya'     ||
+      input == "neat noodles"         ||
+      input == "awesome ayam"         ||
+      input == "delicious dumpling"  
+      ) {
+    myOutputValue = 'you wrote the secret phrase!';
+  }
+  // return myOutputValue as output
+  return myOutputValue;
+};
