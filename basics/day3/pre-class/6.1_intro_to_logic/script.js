3cc7c541361de5d25e90572102cb126c8d89f245
1Regina <1reginacheong@gmail.com>
2021-10-08 22:48:41 +0800

    change var to let

--- basics/day3/pre-class/6.1_intro_to_logic/script.js
+++ basics/day3/pre-class/6.1_intro_to_logic/script.js
@@ -10,9 +10,9 @@
 // };
 
 // Random Dice Gues
-var main = function (input) {
+let main = function (input) {
   // Generate a random dice number
-  var randomDiceNumber = rollDice();
+  let randomDiceNumber = rollDice();
 
   // Default output value is 'you lose'.
   console.log("dice roll-lose case!");
@@ -34,28 +34,28 @@ var main = function (input) {
   return myOutputValue;
 };
 
-var getRandomInteger = function (max) {
+let getRandomInteger = function (max) {
   // Generate a decimal from 0 through max + 1.
   // This decimal will be inclusive of 0 and exclusive of max + 1.
-  var randomDecimal = Math.random() * (max + 1);
+  let randomDecimal = Math.random() * (max + 1);
 
   // Remove the decimal with the floor operation.
   // The resulting integer will be from 0 through max, inclusive of 0 and max.
-  var resultInteger = Math.floor(randomDecimal);
+  let resultInteger = Math.floor(randomDecimal);
 
   return resultInteger;
 };
 
-var rollDice = function () {
+let rollDice = function () {
   // Generate a decimal from 0 through 6, inclusive of 0 and exclusive of 6.
-  var randomDecimal = Math.random() * 6;
+  let randomDecimal = Math.random() * 6;
 
   // Remove the decimal with the floor operation.
   // This will be an integer from 0 to 5 inclusive.
-  var randomInteger = Math.floor(randomDecimal);
+  let randomInteger = Math.floor(randomDecimal);
 
   // Add 1 to get valid dice rolls of 1 through 6 inclusive.
-  var diceNumber = randomInteger + 1;
+  let diceNumber = randomInteger + 1;
 
   return diceNumber;
 };
