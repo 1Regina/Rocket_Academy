858f778e2b1699ae158ae61fbc80688c2f17bcd3
1Regina <1reginacheong@gmail.com>
2021-10-17 14:20:45 +0800

    input validation dice game

---
+++ basics/day5/pre-class/8.3_input_valid/scriptOrg.js
@@ -0,0 +1,35 @@
+var rollDice = function () {
+    // produce a decimal between 0 and 6  
+    var randomDecimal = Math.random() * 6;  
+    // remove the decimal  
+    var randomInteger = Math.floor(randomDecimal);  
+    // add 1 to get a number between 1 and 6 inclusive  
+    var diceNumber = randomInteger + 1;  
+    return diceNumber;
+  };
+  
+  var main = function (input) {  
+    var myOutputValue = '';  
+    
+    // first check if input is a number  
+    if (Number.isNaN(Number(input)) == true) {    
+      myOutputValue = 'sorry please enter a number.';    
+      return myOutputValue;  }  
+      
+    // then check if input is between 1 and 6  
+    if (input < 1 || input > 6) {    
+      myOutputValue = 'sorry please enter a number from 1 - 6';    
+      return myOutputValue;  
+    }  
+    
+    // the input is 1-6, go ahead with the dice game  
+    var randomDiceNumber = rollDice();  
+    
+    if (randomDiceNumber == input) {    
+      myOutputValue = 'you win';    
+      return myOutputValue;  
+    }  
+    
+    myOutputValue = 'you lose';  
+    return myOutputValue;
+  };
