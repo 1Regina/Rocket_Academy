aaa7d05501208c298edf8cf7c6698f28c4a5a59d
1Regina <1reginacheong@gmail.com>
2021-11-21 23:48:04 +0800

    0.3 Documentation button show date

---
+++ Bootcamp/module0/week1/day2/pre-class/DOM_EventListener/script.js
@@ -0,0 +1,10 @@
+const main = (input) => {
+  var today = new Date();
+  var dd = today.getDate()
+  var mm = today.getMonth() + 1
+  var yyyy = today.getFullYear();
+
+  today = mm + '/' + dd + '/' + yyyy;
+  return today;
+}
+
