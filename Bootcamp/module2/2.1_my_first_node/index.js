7ad3814781d4107d2fe76ad71cc0ab9622dc790b
1Regina <1reginacheong@gmail.com>
2022-01-07 23:01:39 +0800

    node.js program

---
+++ Bootcamp/module2/2.1_my_first_node/index.js
@@ -0,0 +1,11 @@
+// console.log('hello');
+
+// let counter = 0;
+
+// while (true) {
+//   counter = counter + 1;
+// }
+
+console.log(process);
+
+// runtime environment: https://www.codecademy.com/articles/introduction-to-javascript-runtime-environments
