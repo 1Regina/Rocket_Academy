05ca6054fc7212cdab590d53277ef32571499262
1Regina <1reginacheong@gmail.com>
2022-01-09 01:28:49 +0800

    node.js bidirectional hex vs rgb converter

--- Bootcamp/module2/2_ICE_conversion/index.js
+++ Bootcamp/module2/2_ICE_conversion/index.js
@@ -47,20 +47,91 @@ if (source === 'celsius' && target === 'Kelvin') {
 
 // FANCY with loops
 let result;
-for (i = 4; i < process.argv.length; i++) {
-  if (source === 'Kelvin' && target === 'farenheit') {
-    result = ((Number(process.argv[i]) - 273.15) * 9) / 5 + 32;
-  }
+const convertKelvin = () => {
+  for (i = 4; i < process.argv.length; i++) {
+    if (source === 'Kelvin' && target === 'farenheit') {
+      result = ((Number(process.argv[i]) - 273.15) * 9) / 5 + 32;
+    }
 
-  if (source === 'farenheit' && target === 'Kelvin') {
-    result = ((Number(process.argv[i]) - 32) * 5) / 9 + 273.15;
-  }
+    if (source === 'farenheit' && target === 'Kelvin') {
+      result = ((Number(process.argv[i]) - 32) * 5) / 9 + 273.15;
+    }
 
-  if (source === 'Kelvin' && target === 'celsius') {
-    result = Number(process.argv[i]) - 273.15;
+    if (source === 'Kelvin' && target === 'celsius') {
+      result = Number(process.argv[i]) - 273.15;
+    }
+    if (source === 'celsius' && target === 'Kelvin') {
+      result = Number(process.argv[i]) + 273.15;
+    }
+    console.log(result);
   }
-  if (source === 'celsius' && target === 'Kelvin') {
-    result = Number(process.argv[i]) + 273.15;
+  return 'All is well';
+};
+// console.log(convertKelvin());
+
+// POST CLASS
+// BASE
+const hexCode = process.argv[2];
+const hexToRgb = (hex) => {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};
+
+// hexCode is process.argv[2], which is the user's input
+// console.log('converting hex to rgb...');
+// console.log(hexToRgb(hexCode));
+
+// Comfortable
+const hexOrRgb = process.argv[2];
+const rgbCode = process.argv[3];
+
+const componentFromStr = (numStr, percent) => {
+  var num = Math.max(0, parseInt(numStr, 10));
+  return percent
+    ? Math.floor((255 * Math.min(100, num)) / 100)
+    : Math.min(255, num);
+};
+
+const rgbHex = (rgb) => {
+  var rgbRegex =
+    /^rgb\(\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*,\s*(-?\d+)(%?)\s*\)$/;
+  var result,
+    r,
+    g,
+    b,
+    hex = '';
+  if ((result = rgbRegex.exec(rgb))) {
+    r = componentFromStr(result[1], result[2]);
+    g = componentFromStr(result[3], result[4]);
+    b = componentFromStr(result[5], result[6]);
+
+    hex = '#' + (0x1000000 + (r << 16) + (g << 8) + b).toString(16).slice(1);
   }
-  console.log(result);
-}
+  return hex;
+};
+
+// rgbCode is process.argv[3], which is the user's input
+// console.log('converting rgb to hex...');
+// console.log(rgbHex(rgbCode));
+
+// Bidirectional conversion
+let output 
+const colorCode = process.argv[3];
+const convertColor = () => {
+  if (hexOrRgb === 'hexToRgb') {
+    console.log('converting hex to rgb...');
+    output = hexToRgb(colorCode);
+  }
+  if (hexOrRgb === 'rgbHex') {
+    console.log('converting rgb to hex...');
+    output = rgbHex(colorCode);
+  }
+  return output;
+};
+console.log(convertColor());
