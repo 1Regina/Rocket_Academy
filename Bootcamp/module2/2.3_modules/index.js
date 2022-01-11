d0d1dd623d956edd7855499200d9588bd4fb939f
1Regina <1reginacheong@gmail.com>
2022-01-11 21:44:24 +0800

    console a module import show all its functions

--- Bootcamp/module2/2.3_modules/index.js
+++ Bootcamp/module2/2.3_modules/index.js
@@ -8,13 +8,12 @@ import {
 import { circleArea } from './modules/area.js';
 import { calculateBill } from './modules/tripCost.js';
 
-//DEFAULT 
+//DEFAULT
 // For default exports, the imported variable's name does not need to
 // match the exported variable's name in the source module.
 // For example, we could replace the following "plus" with "foo".
 import someFunction from './modules/maths.js'; // dont use { } to import exported default function
 import * as maths from './modules/maths.js';
-
 
 console.log(`addition`, add(2, 2));
 console.log(`convert kilometersToMiles`, kilometersToMiles(3));
@@ -23,4 +22,6 @@ console.log(`convert kilogramsToPounds ` + kgToPounds(3));
 console.log(`circleArea`, circleArea(2));
 // console.log(`unexported PI`, PI); // Leads to error
 console.log(`plus from maths`, someFunction(2, 2));
-console.log(`* as maths for subtract` , maths.subtract(5,2))
+console.log(`* as maths for subtract`, maths.subtract(5, 2));
+
+console.log(`maths`, maths); /// gives all the functions within maths
