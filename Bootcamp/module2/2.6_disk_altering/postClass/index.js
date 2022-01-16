69762f01a48964d06cde1953e13b554a04043c62
1Regina <1reginacheong@gmail.com>
2022-01-17 00:00:50 +0800

    model solution for base

--- Bootcamp/module2/2.6_disk_altering/postClass/index.js
+++ Bootcamp/module2/2.6_disk_altering/postClass/index.js
@@ -1,68 +1,53 @@
 import { readFile, writeFile } from 'fs';
 
-const hexToRgb = (hex) => {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return 'converting hex to rgb...' + result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null;
-};
+const filename = 'styles.css';
 
-console.log(`testing now`, hexToRgb('#ffffff'));
-const handleFileRead = (error, content) => {
-  // Handle errors if any
-  if (error) {
-    console.log('read error', error);
-    return;
-  }
-  // work to be done on contents
-
+const convertColorFormats = (content) => {
   const lines = content.split('\n');
   let newLines = '';
-  for (let i = 0; i < lines.length; i++) {
-    if (lines[i].includes('#')) {
-      let code = lines[i].slice(20, 27);
-      console.log(`code is`, code);
-      let hexcode = "'" + code + "'";
-      console.log(`hexcode is`, hexcode);
-      console.log(`hexcode type is`, typeof hexcode);
-      let rgbObject = hexToRgb(hexcode);
-      console.log(`rgbObject is`, rgbObject);
-      // }
+
+  // this is from stackoverflow, not necessary to completely understand right now
+  const convertHexToRGB = (hex) => {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16),
+        }
+      : null;
+  };
+
+  for (let i = 0; i < lines.length; i += 1) {
+    if (lines[i][2] === 'b') {
+      const hexcode = lines[i].slice(20, 27);
+      // convertHexToRGB returns an object
+      const rgbObject = convertHexToRGB(hexcode);
       newLines += `background-color: rgb(${rgbObject.r}, ${rgbObject.g}, ${rgbObject.b});`;
-      lines[i] = lines[i].replace('\r', '');
-      console.log(`background change`, newLines);
     } else {
       newLines += lines[i];
-      lines[i] = lines[i].replace('\r', '');
-      console.log(`same Lines`, newLines);
     }
-
-    // lines[i] = lines[i].replace('\n', '');
-    // lines[i] = lines[i].replace('\r', '');
     newLines += '\n';
-    // see effects to contents
-    console.log(`newLines`, newLines);
-    return newLines;
   }
-
-  // prepare the contents into a text as input to be presented
-  // let allLines = '';
-  // for (let i = 0; i < lines.length; i++) {
-  //   allLines += lines[i] + '\n';
-  // }
-  writeFile(filename, allLines, (writeErr) => {
-    // Catch writing error if any
-    if (writeErr) {
-      console.log('error writing', allLines, writeErr);
-      return;
-    }
-    console.log('success!');
-  });
+  return newLines;
 };
 
-const filename = process.argv[2];
-readFile(filename, 'utf8', handleFileRead);
+const whenFileIsRead = (error, content) => {
+  if (error) {
+    console.log('read error', error);
+  }
+
+  if (process.argv[2] === 'styles.css') {
+    const newContent = convertColorFormats(content);
+
+    writeFile(filename, newContent, (err) => {
+      if (err) {
+        console.log('error writing', newContent, err);
+      } else {
+        console.log('success!');
+      }
+    });
+  }
+};
+
+readFile(filename, 'utf8', whenFileIsRead);
