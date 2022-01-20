6d815ef9f2419fbea18ff1b23e328ae27355753f
1Regina <1reginacheong@gmail.com>
2022-01-20 23:11:57 +0800

    taking bernice solution for alter file

--- Bootcamp/module2/2.6_disk_altering/postClass/index.js
+++ Bootcamp/module2/2.6_disk_altering/postClass/index.js
@@ -1,12 +1,9 @@
 import { readFile, writeFile } from 'fs';
-
-const filename = 'styles.css';
-
-const convertColorFormats = (content) => {
-  const lines = content.split('\n');
-  let newLines = '';
-
-  // this is from stackoverflow, not necessary to completely understand right now
+let filename = process.argv[3];
+const changeColors = (content) => {
+  let lines = content.split('\n');
+  console.log('lines', lines);
+  let newlines = '';
   const convertHexToRGB = (hex) => {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result
@@ -17,37 +14,57 @@ const convertColorFormats = (content) => {
         }
       : null;
   };
-
+  const componentToHex = (c) => {
+    var hex = c.toString(16);
+    return hex.length == 1 ? '0' + hex : hex;
+  };
+  const rgbToHex = (string) => {
+    let array = changeRgbString(string);
+    console.log(array);
+    return (
+      '#' +
+      componentToHex(Number(array[0])) +
+      componentToHex(Number(array[1])) +
+      componentToHex(Number(array[2]))
+    );
+  };
+  const changeRgbString = (string) => {
+    let newstring = string.replace('(', '');
+    newstring = newstring.replace(')', '');
+    newstring = newstring.replace(';', '');
+    newstring = newstring.split(',');
+    return newstring;
+  };
   for (let i = 0; i < lines.length; i += 1) {
-    if (lines[i][2] === 'b') {
-      const hexcode = lines[i].slice(20, 27);
-      // convertHexToRGB returns an object
-      const rgbObject = convertHexToRGB(hexcode);
-      newLines += `background-color: rgb(${rgbObject.r}, ${rgbObject.g}, ${rgbObject.b});`;
+    if (lines[i][2] == 'b' && process.argv[2] === 'hextorgb') {
+      let hexColor = lines[i].slice(20, 27);
+      let rgbColor = convertHexToRGB(hexColor);
+      newlines += `  background-color: rgb(${rgbColor.r},${rgbColor.g},${rgbColor.b})`;
+    } else if (lines[i][2] == 'b' && process.argv[2] === 'rgbtohex') {
+      let rgbColor = lines[i].slice(23, 37);
+      let hexColor = rgbToHex(rgbColor);
+      newlines += `  background-color: ${hexColor}`;
     } else {
-      newLines += lines[i];
+      newlines += lines[i];
     }
-    newLines += '\n';
+    newlines += '\n';
   }
-  return newLines;
+  return newlines;
 };
-
-const whenFileIsRead = (error, content) => {
+const fileRead = (error, content) => {
   if (error) {
-    console.log('read error', error);
+    console.log('errror occured');
   }
-
-  if (process.argv[2] === 'styles.css') {
-    const newContent = convertColorFormats(content);
-
-    writeFile(filename, newContent, (err) => {
+  if (filename == 'styles.css') {
+    const newcontent = changeColors(content);
+    console.log(newcontent);
+    writeFile(filename, newcontent, (err) => {
       if (err) {
-        console.log('error writing', newContent, err);
+        console.log('error when writing');
       } else {
-        console.log('success!');
+        console.log('sucessfully saved');
       }
     });
   }
 };
-
-readFile(filename, 'utf8', whenFileIsRead);
+readFile(filename, 'utf8', fileRead);
