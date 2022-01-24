fefe15721745ded99f226ea4a0399cdb2887ff7b
1Regina <1reginacheong@gmail.com>
2022-01-24 22:17:16 +0800

    base solution from Liam for modify deck n deal cards

--- Bootcamp/module2/2_ICE5_inclass/index.js
+++ Bootcamp/module2/2_ICE5_inclass/index.js
@@ -1,292 +1,40 @@
-import { write, read, add } from './jsonFileStorage.js';
-import { writeFile, readFile } from 'fs';
+import { readFile, writeFile } from 'fs';
+import { add, read, write } from './jsonFileStorage.js';
+import { shuffleCards, deal } from './customFunctions.js';
+import cardDeck from './deck.js';
 
-// Hard code deck
-const deck = [
-  {
-    name: 'ace',
-    suit: 'hearts',
-    rank: 1,
-  },
-  {
-    name: '2',
-    suit: 'hearts',
-    rank: 2,
-  },
-  {
-    name: '3',
-    suit: 'hearts',
-    rank: 3,
-  },
-  {
-    name: '4',
-    suit: 'hearts',
-    rank: 4,
-  },
-  {
-    name: '5',
-    suit: 'hearts',
-    rank: 5,
-  },
-  {
-    name: '6',
-    suit: 'hearts',
-    rank: 6,
-  },
-  {
-    name: '7',
-    suit: 'hearts',
-    rank: 7,
-  },
-  {
-    name: '8',
-    suit: 'hearts',
-    rank: 8,
-  },
-  {
-    name: '9',
-    suit: 'hearts',
-    rank: 9,
-  },
-  {
-    name: '10',
-    suit: 'hearts',
-    rank: 10,
-  },
-  {
-    name: 'jack',
-    suit: 'hearts',
-    rank: 11,
-  },
-  {
-    name: 'queen',
-    suit: 'hearts',
-    rank: 12,
-  },
-  {
-    name: 'king',
-    suit: 'hearts',
-    rank: 13,
-  },
-  {
-    name: 'ace',
-    suit: 'diamonds',
-    rank: 1,
-  },
-  {
-    name: '2',
-    suit: 'diamonds',
-    rank: 2,
-  },
-  {
-    name: '3',
-    suit: 'diamonds',
-    rank: 3,
-  },
-  {
-    name: '4',
-    suit: 'diamonds',
-    rank: 4,
-  },
-  {
-    name: '5',
-    suit: 'diamonds',
-    rank: 5,
-  },
-  {
-    name: '6',
-    suit: 'diamonds',
-    rank: 6,
-  },
-  {
-    name: '7',
-    suit: 'diamonds',
-    rank: 7,
-  },
-  {
-    name: '8',
-    suit: 'diamonds',
-    rank: 8,
-  },
-  {
-    name: '9',
-    suit: 'diamonds',
-    rank: 9,
-  },
-  {
-    name: '10',
-    suit: 'diamonds',
-    rank: 10,
-  },
-  {
-    name: 'jack',
-    suit: 'diamonds',
-    rank: 11,
-  },
-  {
-    name: 'queen',
-    suit: 'diamonds',
-    rank: 12,
-  },
-  {
-    name: 'king',
-    suit: 'diamonds',
-    rank: 13,
-  },
-  {
-    name: 'ace',
-    suit: 'clubs',
-    rank: 1,
-  },
-  {
-    name: '2',
-    suit: 'clubs',
-    rank: 2,
-  },
-  {
-    name: '3',
-    suit: 'clubs',
-    rank: 3,
-  },
-  {
-    name: '4',
-    suit: 'clubs',
-    rank: 4,
-  },
-  {
-    name: '5',
-    suit: 'clubs',
-    rank: 5,
-  },
-  {
-    name: '6',
-    suit: 'clubs',
-    rank: 6,
-  },
-  {
-    name: '7',
-    suit: 'clubs',
-    rank: 7,
-  },
-  {
-    name: '8',
-    suit: 'clubs',
-    rank: 8,
-  },
-  {
-    name: '9',
-    suit: 'clubs',
-    rank: 9,
-  },
-  {
-    name: '10',
-    suit: 'clubs',
-    rank: 10,
-  },
-  {
-    name: 'jack',
-    suit: 'clubs',
-    rank: 11,
-  },
-  {
-    name: 'queen',
-    suit: 'clubs',
-    rank: 12,
-  },
-  {
-    name: 'king',
-    suit: 'clubs',
-    rank: 13,
-  },
-  {
-    name: 'ace',
-    suit: 'spades',
-    rank: 1,
-  },
-  {
-    name: '2',
-    suit: 'spades',
-    rank: 2,
-  },
-  {
-    name: '3',
-    suit: 'spades',
-    rank: 3,
-  },
-  {
-    name: '4',
-    suit: 'spades',
-    rank: 4,
-  },
-  {
-    name: '5',
-    suit: 'spades',
-    rank: 5,
-  },
-  {
-    name: '6',
-    suit: 'spades',
-    rank: 6,
-  },
-  {
-    name: '7',
-    suit: 'spades',
-    rank: 7,
-  },
-  {
-    name: '8',
-    suit: 'spades',
-    rank: 8,
-  },
-  {
-    name: '9',
-    suit: 'spades',
-    rank: 9,
-  },
-  {
-    name: '10',
-    suit: 'spades',
-    rank: 10,
-  },
-  {
-    name: 'jack',
-    suit: 'spades',
-    rank: 11,
-  },
-  {
-    name: 'queen',
-    suit: 'spades',
-    rank: 12,
-  },
-  {
-    name: 'king',
-    suit: 'spades',
-    rank: 13,
-  },
-];
-const filename = 'data.json';
-const modifyJsonFile = (filename) => {
+const action = process.argv[2];
+
+const modifyJsonFile = (filename, key) => {
   const handleFileRead = (readErr, jsonContentStr) => {
     if (readErr) {
       console.log('Reading error', readErr);
     }
 
     // Convert data from string to Object
-    const jsonContentObj = JSON.parse(jsonContentStr);
-    console.log(`jsonContentObj`, jsonContentObj);
-    // TODO: Modify the data however we would like
-    const random = Math.floor(Math.random() * 52);
-    const cardToRemove = Object.values(jsonContentObj)[random];
-    console.log(`cardToRemove`, cardToRemove);
-    const modiDeck = Object.values(jsonContentObj).splice(random, 1);
-    console.log(`type of modiDeck`, typeof modiDeck);
-    // Add the new key and value to the content object.
-    const newjsonContentObj = {};
-    newjsonContentObj[`newCardDeck`] = modiDeck;
+    let jsonContentObj = JSON.parse(jsonContentStr);
+    const deckToModify = jsonContentObj[key];
+
+    // Alter the Deck - Deal Cards
+    if (action === 'alter') {
+      const removedCard = deckToModify.pop();
+      console.log(removedCard);
+    }
+
+    // Shuffle Existing Card Deck
+    if (action === 'shuffle') {
+      jsonContentObj[key] = shuffleCards(deckToModify);
+    }
+
+    // Deal Cards from Deck
+    if (action === 'deal') {
+      const freshDeck = shuffleCards(deckToModify)
+      const currrentHand = deal(freshDeck, 2);
+      console.log(currrentHand);
+    }
 
     // Convert data from Object to string
-    // const updatedJsonContentStr = JSON.stringify(jsonContentObj);
-    const updatedJsonContentStr = JSON.stringify(newjsonContentObj);
+    const updatedJsonContentStr = JSON.stringify(jsonContentObj);
 
     // Write updated data to file
     writeFile(filename, updatedJsonContentStr, (writeErr) => {
@@ -300,51 +48,20 @@ const modifyJsonFile = (filename) => {
   readFile(filename, 'utf-8', handleFileRead);
 };
 
-// Get a random index ranging from 0 (inclusive) to max (exclusive).
-var getRandomIndex = function (max) {
-  return Math.floor(Math.random() * max);
-};
+// Write New Card Deck
+if (action === 'create') {
+  add('data.json', 'cardDeck', cardDeck);
+}
 
-// Shuffle the elements in the cardDeck array
-var shuffleCards = function (cardDeck) {
-  // Loop over the card deck array once
-  var currentIndex = 0;
-  while (currentIndex < cardDeck.length) {
-    // Select a random index in the deck
-    var randomIndex = getRandomIndex(cardDeck.length);
-    // Select the card that corresponds to randomIndex
-    var randomCard = cardDeck[randomIndex];
-    // Select the card that corresponds to currentIndex
-    var currentCard = cardDeck[currentIndex];
-    // Swap positions of randomCard and currentCard in the deck
-    cardDeck[currentIndex] = randomCard;
-    cardDeck[randomIndex] = currentCard;
-    // Increment currentIndex
-    currentIndex = currentIndex + 1;
-  }
-  // Return the shuffled deck
-  return cardDeck;
-};
+// Read Cards From Deck
+if (action === 'read') {
+  read('data.json');
+}
 
-const value = deck;
-const doSomething = () => {
-  // Write New Card Deck
-  if (process.argv[2] === 'create') {
-    add('data.json', 'cardDeck', value);
-  }
-  // Read Cards From Deck
-  if (process.argv[2] === 'read') {
-    read('data.json');
-  }
-  // Alter the deck
-  if (process.argv[2] === 'modify') {
-    modifyJsonFile('data.json');
-  }
-  // Shufflethe deck
-  // if (process.argv[2] === 'shuffle) {
+if (action === 'alter' || action === 'shuffle' || action === 'deal') {
+  modifyJsonFile('data.json', 'cardDeck');
+}
 
-  //   modifyJsonFile('data.json');
-  // }
-};
-
-doSomething();
+if (action === 'reset') {
+  write('data.json', {});
+}
