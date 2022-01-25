8ba0f02704f066064104e67f075b94523f731c28
1Regina <1reginacheong@gmail.com>
2022-01-25 20:46:53 +0800

    edit info in file with callbacks

---
+++ Bootcamp/module2/2.13.3_json_storage_summary/index.js
@@ -0,0 +1,40 @@
+import { add, edit } from './jsonFileStorage.js';
+
+// If the user specifies add functionality
+if (process.argv[2] === 'add') {
+  // Name and address are 2nd and 3rd parameters respectively
+  const name = process.argv[3];
+  const address = process.argv[4];
+
+  // Create JS Object of data to store in DB
+  const person = {
+    name: name,
+    address: address,
+  };
+
+  // Tell jsonFileStorage to store new person in the people array in data.json.
+  // The callback is optional, and allows us to perform logic with updated data,
+  // e.g. update the UI.
+  add('data.json', 'people', person, (err) => {
+    if (!err) {
+      console.log('Done!');
+    }
+  });
+}
+if (process.argv[2] === 'edit') {
+  edit(
+    'data.json',
+    (err, jsonContentObj) => {
+      // If no error, edit the content
+      if (!err) {
+        jsonContentObj['people'][0].name = 'Terminator';
+      }
+    },
+    (err) => {
+      // If no error, edit the content
+      if (!err) {
+       console.log("lihllhihibhaidy")
+      }
+    }
+  );
+}
