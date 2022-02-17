aff05ca9c7ea3a35c12ceec2ef7207c5ac408604
1Regina <1reginacheong@gmail.com>
2022-02-17 22:35:00 +0800

    add post method to add sightings to data.json with - curl - index.js - thunderclient

--- Bootcamp/module3/3ICE3_BigFoot/index.js
+++ Bootcamp/module3/3ICE3_BigFoot/index.js
@@ -12,10 +12,10 @@ const port = 3004
 app.set('view engine', 'ejs');
 
 // app.get('/healthcheck', (req, res) => res.send('I M OK'))
-// Save new recipe data sent via POST request from our form
+// Save new sighting data sent via POST request from our form
 app.post('/bigfoot', (request, response) => {
 
-  // Add new recipe data in request.body to sightings array in data.json.
+  // Add new sighting data in request.body to sightings array in data.json.
   console.log('before added sighting')
   add('data.json', 'sightings', request.body, (err) => {
     console.log('added sighting')
@@ -23,10 +23,9 @@ app.post('/bigfoot', (request, response) => {
       response.status(500).send('DB write error.');
       return;
     }
-    // Acknowledge recipe saved.
+    // Acknowledge sighting saved.
     return response.send('Saved bigfoot sighting!');
   });
-  // response.send('d;kdfg!');
 });
 
 // Render the form to input new sightings
@@ -36,20 +35,42 @@ app.get('/bigfoot', (request, response) => {
 
 
 // for practice with curl $ curl -d "YEAR=1989&SEASON=spring" -X POST http://localhost:3004/sighting
+// For Thunderclient, use request.query
 app.post('/sighting', (request, response) => {
 
-  // Add new recipe data in request.body to sightings array in data.json.
+  // Add new sighting data in request.query to sightings array in data.json.
   console.log('before added sighting')
-  add('data.json', 'sightings', request.body, (err) => {
+  add('data.json', 'sightings', request.query, (err) => {
     console.log('added sighting')
     if (err) {
       response.status(500).send('DB write error.');
       return;
     }
-    // Acknowledge recipe saved.
+    // Acknowledge sighting saved.
     return response.send('Saved bigfoot sighting!');
   });
   // response.send('d;kdfg!');
 });
+
+// Render the form to input new sightings
+app.get('/sighting', (request, response) => {
+  response.render('sighting');
+});
+
+// app.post('/sighting', (request, response) => {
+
+//   // Add new recipe data in request.body to sightings array in data.json.
+//   console.log('before added sighting')
+//   add('data.json', 'sightings', request.body, (err) => {
+//     console.log('added sighting')
+//     if (err) {
+//       response.status(500).send('DB write error.');
+//       return;
+//     }
+//     // Acknowledge recipe saved.
+//     return response.send('Saved bigfoot sighting!');
+//   });
+//   // response.send('d;kdfg!');
+// });
 
 app.listen(port)
