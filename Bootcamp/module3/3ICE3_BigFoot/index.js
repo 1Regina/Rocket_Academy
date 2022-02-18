2e20ce8d92422f7b024c7e3500d303ed32479ec2
1Regina <1reginacheong@gmail.com>
2022-02-18 17:00:31 +0800

    COMFORTABLE in bigfoot sighting to redirect to page to show last record

--- Bootcamp/module3/3ICE3_BigFoot/index.js
+++ Bootcamp/module3/3ICE3_BigFoot/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {add} from './jsonFileStorage.js';
+import read, {add} from './jsonFileStorage.js';
 
 const app = express();
 app.use(express.static('public'));
@@ -12,7 +12,8 @@ const port = 3004
 app.set('view engine', 'ejs');
 
 // app.get('/healthcheck', (req, res) => res.send('I M OK'))
-// Save new sighting data sent via POST request from our form
+// Save new sighting data sent via POST request from our form. 
+// BASE. default in sighting.ejs is <form action="/bigfoot" method="POST"></form>
 app.post('/bigfoot', (request, response) => {
 
   // Add new sighting data in request.body to sightings array in data.json.
@@ -53,24 +54,60 @@ app.post('/sighting', (request, response) => {
 });
 
 // Render the form to input new sightings
-app.get('/sighting', (request, response) => {
+// app.get('/sighting', (request, response) => {
+//   response.render('sighting');
+// });
+
+// COMFORTABLE - redirect to http://localhost:3004/sighting/<INDEX>
+//!! need to update in sighting.ejs <form action="/bigfoot_new_record" method="POST">
+// Render the form to input new sightings
+app.get('/bigfoot_new_record', (request, response) => {
   response.render('sighting');
 });
+// Save new sighting data sent via POST request from our form
+app.post('/bigfoot_new_record', (request, response) => {
+  // Add new sighting data in request.body to sightings array in data.json.
+  console.log('before added sighting')
+  add('data.json', 'sightings', request.body, (err) => {
+    console.log('added sighting')
+    if (err) {
+      response.status(500).send('DB write error.');
+      return;
+    }
+  });
+  // console.log(`safdsafdasgaregrav`)
+    // Acknowledge sighting saved.
+    // return response.send('Saved bigfoot sighting!');
+  // Redirect to new recording
+  read(`data.json`, (error, jsonObjContent) => {
+    if (error) {
+      console.error(`read error`, error);
+      return;
+    }
+    const data = jsonObjContent.sightings;
+    // console.log(`data`,data)
+    // const total = data.length
+    let index = data.length 
+    console.log(`last index`, index)
+    response.redirect(`/sightings/${index}`) 
+    });
+})
 
-// app.post('/sighting', (request, response) => {
 
-//   // Add new recipe data in request.body to sightings array in data.json.
-//   console.log('before added sighting')
-//   add('data.json', 'sightings', request.body, (err) => {
-//     console.log('added sighting')
-//     if (err) {
-//       response.status(500).send('DB write error.');
-//       return;
-//     }
-//     // Acknowledge recipe saved.
-//     return response.send('Saved bigfoot sighting!');
-//   });
-//   // response.send('d;kdfg!');
-// });
+app.get('/sightings/:index', (req,res) =>{
+  read(`data.json`, (error, jsonObjContent) => {
+    if (error) {
+      console.error(`read error`, error);
+      return;
+    }
+    const data = jsonObjContent.sightings;
+    // console.log(`data`,data)
+    // const total = data.length
+    const details = jsonObjContent.sightings[req.params.index]
+    console.log(`details`, details)
+    res.render(`single_sighting`, {details})
+  })
+  })
+
 
 app.listen(port)
