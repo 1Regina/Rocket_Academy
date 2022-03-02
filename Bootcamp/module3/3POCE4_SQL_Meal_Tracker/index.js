dfe882229577e091a0adc12288c407b224c4fa38
1Regina <1reginacheong@gmail.com>
2022-03-02 22:41:42 +0800

    meal_tracker COMFORTABLE UPDATE report customize, Edit and REPORT

--- Bootcamp/module3/3POCE4_SQL_Meal_Tracker/index.js
+++ Bootcamp/module3/3POCE4_SQL_Meal_Tracker/index.js
@@ -54,33 +54,33 @@ if (command === 'log') {
 //   client.query(sqlQuery, whenQueryDone);
 // }
 
-// // =============COMFORTABLE UPDATE report customize =========
-// // create the query done callback
-// const whenHungryQueryDone = (error, result) => {
-//     // this error is anything that goes wrong with the query
-//     if (error) {
-//       console.log('error', error);
-//     } else {
-//       // rows key has the data
-//       // BASIE
-//       // console.log(`report results`, result.rows);
-//       /////@@@@@@@/////
-//       // COMFORTABLE -Sample Report Output
-//       let reportResults = result.rows;
-//       let ifHungry = "";
-//       for (i=0; i< reportResults.length; i+=1){
-//         if(reportResults[i].was_hungry_before_eating === true){
-//         ifHungry = "feeling_hungry"
-//       } else {
-//         ifHungry = "feeling stuffed"
-//       }
-//         console.log(`${reportResults[i].id} - ${reportResults[i].type} - ${reportResults[i].description} - ${reportResults[i].amount_of_alcohol} - ${ifHungry}`)
-//       }
-//       console.log(`input Data`, inputData)
-//     }
-//     // close the connection
-//     client.end();
-// };
+// =============COMFORTABLE UPDATE part A report customize =========
+// create the query done callback
+const whenHungryQueryDone = (error, result) => {
+    // this error is anything that goes wrong with the query
+    if (error) {
+      console.log('error', error);
+    } else {
+      // rows key has the data
+      // BASIE
+      // console.log(`report results`, result.rows);
+      /////@@@@@@@/////
+      // COMFORTABLE -Sample Report Output
+      let reportResults = result.rows;
+      let ifHungry = "";
+      for (i=0; i< reportResults.length; i+=1){
+        if(reportResults[i].was_hungry_before_eating === true){
+        ifHungry = "feeling_hungry"
+      } else {
+        ifHungry = "feeling stuffed"
+      }
+        console.log(`${reportResults[i].id} - ${reportResults[i].type} - ${reportResults[i].description} - ${reportResults[i].amount_of_alcohol} - ${ifHungry}`)
+      }
+      console.log(`input Data`, inputData)
+    }
+    // close the connection
+    client.end();
+};
 
 // if (command === 'report') {
 //   sqlQuery = 'SELECT * FROM meal_tracker';
@@ -88,29 +88,83 @@ if (command === 'log') {
 // }
 
 
-// // =============COMFORTABLE UPDATE Edit =========
-// const editDetails = process.argv.slice(3,6);
-// // let editID = process.argv[3];
-// // let editColumn = process.argv[4];
-// // let editValue = process.argv [5];
+// =============COMFORTABLE UPDATE Part B Edit =========
+const editDetails = process.argv.slice(3,6);
+// let editID = process.argv[3];
+// let editColumn = process.argv[4];
+// let editValue = process.argv [5];
 
-// const whenEditQueryDone = (error, result) => {
-//   // this error is anything that goes wrong with the query
-//   if (error) {
-//     console.log(`error`, error);
-//   } else {
-//     console.log(`results`, result.rows);
-//     console.log(`editDetails`, editDetails);
-//   }
-//     // close the connection
-//     client.end();
-// }
+const whenEditQueryDone = (error, result) => {
+  // this error is anything that goes wrong with the query
+  if (error) {
+    console.log(`error`, error);
+  } else {
+    console.log(`results`, result.rows);
+    console.log(`editDetails`, editDetails);
+  }
+    // close the connection
+    client.end();
+}
 
-// if (command === 'edit') {
-//   console.log(editDetails)
-//   const sqlQuery = `UPDATE meal_tracker SET ${editDetails[1]} = '${editDetails[2]}' WHERE id = ${editDetails[0]}`;
-//   // const sqlQuery = `UPDATE meal_tracker SET ${process.argv[4]} = '${process.argv[5]}' WHERE id=${process.argv[3]}`;
-//   client.query(sqlQuery, whenEditQueryDone);
-// }  
+if (command === 'edit') {
+  console.log(editDetails)
+  const sqlQuery = `UPDATE meal_tracker SET ${editDetails[1]} = '${editDetails[2]}' WHERE id = ${editDetails[0]}`;
+  // const sqlQuery = `UPDATE meal_tracker SET ${process.argv[4]} = '${process.argv[5]}' WHERE id=${process.argv[3]}`;
+  client.query(sqlQuery, whenEditQueryDone);
+}  
 
 
+// =============COMFORTABLE REPORT by attribute=========
+// COMFORTABLE REPORT by attribute
+// a function to filter array of objects with string
+function filterByValue(array, string) {
+    return array.filter(o =>
+        Object.keys(o).some(k => String(o[k]).toLowerCase().includes(String(string).toLowerCase())));
+}
+
+let attributeWord = "";
+if (process.argv[3] === "hungry") {
+  attributeWord = true
+} else if (process.argv[3] === "not-hungry"){
+  attributeWord = false
+} else {
+  attributeWord = process.argv[3]
+}
+
+// make boolean in was_hungry_before_eating to a string
+const formatHungryMeals = (resultArray) => {
+  let ifHungry = "";
+  for (i=0; i< resultArray.length; i+=1){
+    if(resultArray[i].was_hungry_before_eating === true) {
+      ifHungry = "feeling hungry"
+    } else {
+      ifHungry = "feeling not hungry"
+    }
+    console.log(`${resultArray[i].id} - ${resultArray[i].type} - ${resultArray[i]. description} - ${resultArray[i].amount_of_alcohol} - ${ifHungry}`)
+  }
+}
+
+const whenReportQueryAttribute = (error, result) => {
+  let filteredRecords; 
+  // this error is anything that goes wrong with the query
+  if (error) {
+    console.log(`error`, error);
+  } else {
+  // console.log(`result.rows output`, result.rows)
+  filteredRecords =  filterByValue(result.rows, attributeWord);
+  }
+  // Method 1 output table contents as is
+  console.log (`filtered Records`, filteredRecords)
+
+  // Method 2 outputing via helper function replace boolean
+  formatHungryMeals(filteredRecords);
+
+  // close the connection
+  client.end();
+
+}
+
+if (command === "report") {
+  const sqlQuery = `SELECT * FROM meal_tracker`;
+  client.query(sqlQuery, whenReportQueryAttribute);
+}
